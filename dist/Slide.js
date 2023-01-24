5e4dbd7209698473df293a2f6ce6e2b2764c1f5a
Roberto Conrado <robertoconradorc@gmail.com>
2023-01-24 13:55:59 -0300

    trabalhando-no-HTMLVideoElement

--- dist/Slide.js
+++ dist/Slide.js
@@ -23,13 +23,27 @@ export default class Slide {
     }
     hide(el) {
         el.classList.remove("active");
+        if (el instanceof HTMLVideoElement) {
+            el.currentTime = 0;
+            el.pause();
+        }
     }
     show(index) {
         this.index = index;
         this.slide = this.slides[this.index];
         this.slides.forEach((el) => this.hide(el));
         this.slide.classList.add("active");
-        this.auto(this.time);
+        if (this.slide instanceof HTMLVideoElement) {
+            this.autoVideo(this.slide);
+        }
+        else {
+            this.auto(this.time);
+        }
+    }
+    autoVideo(video) {
+        video.muted = true;
+        video.play();
+        this.auto(video.duration * 1000);
     }
     auto(time) {
         this.timeout?.clear();
@@ -48,18 +62,20 @@ export default class Slide {
         this.show(next);
     }
     pause() {
-        console.log('pause');
         this.pausedTimeout = new Timeout(() => {
             this.timeout?.pause();
             this.paused = true;
+            if (this.slide instanceof HTMLVideoElement)
+                this.slide.pause();
         }, 300);
     }
     continue() {
-        console.log('continue');
         this.pausedTimeout?.clear();
         if (this.paused) {
             this.paused = false;
             this.timeout?.continue();
+            if (this.slide instanceof HTMLVideoElement)
+                this.slide.play();
         }
     }
     addControls() {
